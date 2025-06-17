8db364e468c0e2e18a457727ab8e033b5a0da6aa
FaridG7 <goharifarid7@gmail.com>
2025-06-18 03:04:14 +0330

    added tagged_keys logic and fixed a bug

--- modules/MessageHandler.py
+++ modules/MessageHandler.py
@@ -18,7 +18,7 @@ class MessageHandler(AES, RSA):
             'reciever_public_key_pem':reciever_public_key_pem
         }
     
-    def generate_random_text(length):
+    def generate_random_text(length:int):
         chars = string.ascii_letters + string.digits
         return ''.join(secrets.choice(chars) for _ in range(length))
     
@@ -30,17 +30,24 @@ class MessageHandler(AES, RSA):
             msg = self.create_message_object(random_text, sender_signature, reciever_public_key_pem)
             fake_messages.append(msg)
 
-    def send_messages(self, messages:list[Message], sender_signature:Signature, reciever_public_key_pem:bytes, users_count:int):
+    def dump_messages_to_file(self, messages:int):
+        filename = self.generate_random_text(random.randint(10, 20))
+        path = Path('files/messages') / f"{filename}.txt"
+        with open(path) as f:
+            for message in messages:
+                tagged_key_and_iv = message['key'] + message['iv'] + b"::OK"
+                encrypted_key = self.encrypt_with_public_key(message['reciever_public_key_pem'], tagged_key_and_iv)
+                cipher_text = self.encrypt(message['message_text'], message['key'], message['iv'])
+                f.write('------------------------------Begin Message------------------------------')
+                f.write(encrypted_key)
+                f.write(message['sender_signature']['payload'])
+                f.write(message['sender_signature']['signature'])
+                f.write(cipher_text)
+                f.write('------------------------------End Message------------------------------')
+
+    def send_messages(self, messages:list[Message], sender_signature:Signature, reciever_public_key_pem:bytes, users_count:int)->None:
         messages_max_length = max(len(message['message_text']) for message in messages)
         messages_min_length = min(len(message['message_text']) for message in messages)
         fake_messages = self.generate_fake_messages(messages_min_length, messages_max_length, sender_signature, reciever_public_key_pem, users_count - len(messages))
         real_and_fake_messages = random.shuffle(messages + fake_messages)
-
-        for message in real_and_fake_messages:
-            cipher_text = self.encrypt(message['message_text'], message['key'], message['iv'])
-            file_name = self.encrypt_with_public_key(message['reciever_public_key_pem'], message['key'] + message['iv'])
-            path = Path('files/messages') / f"{file_name}.txt"
-            with open(path) as f:
-                f.write(message['sender_signature']['payload'])
-                f.write(message['sender_signature']['signature'])
-                f.write(cipher_text)
+        self.dump_messages_to_file(real_and_fake_messages)
